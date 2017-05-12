565136dcbb5f3e2b92e96bcd4c4dd790e0901d49
scosmaa <scosmaa@gmail.com>
2017-05-12 16:49:15 +0200

    First Commit

---
+++ js/online-checker.js
@@ -0,0 +1,3 @@
+setInterval(function(){
+    console.log(window.navigator.onLine);
+}, 1000)
---
+++ service-worker.js
@@ -0,0 +1,45 @@
+var CACHE_NAME = 'offline-cache';
+
+// Files required to make this app work offline
+var REQUIRED_FILES = [
+  'js/online-checker.js',
+  'index.html',
+  '/'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install step:  loading each required file into cache
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        // Add all offline dependencies to the cache
+        return cache.addAll(REQUIRED_FILES);
+      })
+      .then(function() {
+      	// At this point everything has been cached
+        return self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return the response from the cached version
+        if (response) {
+          return response;
+        }
+
+        // Not in cache - return the result from the live server
+        // `fetch` is essentially a "fallback"
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  // Calling claim() to force a "controllerchange" event on navigator.serviceWorker
+  event.waitUntil(self.clients.claim());
+});
